61b442a438a3b4685f517266ba3da72e5f09c6cf
Daeho Cho <cdh981009@gmail.com>
2023-09-13 16:55:42 +0900

    Draw 2 triangles each using different VBO and VAO

--- main.cpp
+++ main.cpp
@@ -101,25 +101,34 @@ int main(void) {
 		1, 2, 3, // second triangle
 	};
 	
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
+	unsigned int VBO1, VBO2;
+	glGenBuffers(1, &VBO1);
+	glGenBuffers(1, &VBO2);
 	unsigned int EBO;
 	glGenBuffers(1, &EBO);
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
+	unsigned int VAO1, VAO2;
+	glGenVertexArrays(1, &VAO1);
+	glGenVertexArrays(1, &VAO2);
 
 	// :: Initialization code ::
 	// 1. bind Vertex Array Object
-	glBindVertexArray(VAO);
+	glBindVertexArray(VAO1);
 	// 2. copy our vertices array in a buffer for OpenGL to use	
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) / 2, vertices, GL_STATIC_DRAW);
 	// 3. copy our indices array in an element buffer for OpenGL to use
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	// 4. then set the vertex attributes pointers
 	// this tells OpenGL how our vertices array is structured
 	// so the vertex shader can work as intended
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	
+	// bing second VBO and VAO
+	glBindVertexArray(VAO2);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) / 2, vertices + 9, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
@@ -135,9 +144,13 @@ int main(void) {
 		// use our shader program when we want to redner an object
 		glUseProgram(shaderProgram);
 		// bind VAO which indirectly binds VBO and vertex attributes
-		glBindVertexArray(VAO);
+		glBindVertexArray(VAO1);
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glBindVertexArray(VAO2);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glBindVertexArray(0); // unbind
 
 		// check and call events and swap the buffers
