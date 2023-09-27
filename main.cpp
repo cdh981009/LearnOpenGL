0e9b4cbe5fc39432fe2acbc7bf51d54770c18411
Daeho Cho <cdh981009@gmail.com>
2023-09-27 23:02:57 +0900

    Generate texture

--- main.cpp
+++ main.cpp
@@ -43,6 +43,30 @@ int main(void) {
     //	1, 2, 3, // second triangle
     //};
     
+    // generate a texture and bound it
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set options
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // texture loading
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("./resources/container.jpg", &width, &height, &nrChannels, 0);
+    if (data) {
+        // generate a texture using the previously loaded image data
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+  
+    // after generating the texture and mipmaps, free the image memory
+    stbi_image_free(data);
+    
     unsigned int VBO;
     glGenBuffers(1, &VBO);
     /*unsigned int EBO;
