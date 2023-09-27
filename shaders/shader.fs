1d0df2938a73dba0994aaaf0024c98e97d04f9bb
Daeho Cho <cdh981009@gmail.com>
2023-09-28 00:43:59 +0900

    Flip only the face using fragment shader

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -10,5 +10,6 @@ uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    vec2 flippedCoord = vec2(-TexCoord.x, -TexCoord.y);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, flippedCoord), 0.2);
 }
