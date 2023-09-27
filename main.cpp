573bede328dcc6a958429bbf76418811e13e26e6
Daeho Cho <cdh981009@gmail.com>
2023-09-28 00:47:21 +0900

    Flip texture coord properly

--- main.cpp
+++ main.cpp
@@ -78,10 +78,10 @@ int main(void) {
     // rectangle
     float vertices[] = {
         // positions          // colors           // texture coords
-         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 2.0f,   // top right
          0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
         -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 2.0f    // top left 
     };
     unsigned int indices[] = { // note that we start from 0!
     	0, 1, 3, // first triangle
--- shaders/shader.fs
+++ shaders/shader.fs
@@ -10,6 +10,6 @@ uniform sampler2D texture2;
 
 void main()
 {
-    vec2 flippedCoord = vec2(-TexCoord.x, -TexCoord.y);
+    vec2 flippedCoord = vec2(1.0 -TexCoord.x, 1.0 -TexCoord.y);
     FragColor = mix(texture(texture1, TexCoord), texture(texture2, flippedCoord), 0.2);
 }
