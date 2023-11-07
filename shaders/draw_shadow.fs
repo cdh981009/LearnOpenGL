7949484666a71c26fb06a1df1ee82fdfae46c3bf
Daeho Cho <cdh981009@gmail.com>
2023-11-07 20:09:11 +0900

    Apply basic PCF

--- shaders/draw_shadow.fs
+++ shaders/draw_shadow.fs
@@ -21,11 +21,23 @@ float calculateShadow(vec4 fragPosLightSpace, vec3 norm, vec3 lightDir) {
 	vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
 	// transform from [-1, 1] NDC to [0, 1] for texture sampling
 	projCoords = projCoords * 0.5 + 0.5;
-	float closestDepth = texture(shadowMap, projCoords.xy).r;
 	float currentDepth = projCoords.z;
-	float bias = max(0.005 * (1.0 - dot(norm, lightDir)), 0.001);
-	float shadow = currentDepth > closestDepth + bias ? 1.0 : 0.0;
-	if (projCoords.z > 1.0) shadow = 0.0;
+	float shadow = 0.0f;
+
+	if (currentDepth > 1.0) {
+		shadow = 0.0;
+	} else {
+		vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+		float bias = max(0.005 * (1.0 - dot(norm, lightDir)), 0.001);
+		for(int x = -1; x <= 1; ++x) {
+			for(int y = -1; y <= 1; ++y) {
+				float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
+				shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0;        
+			}    
+		}
+		shadow /= 9.0;
+	}
+
 	return shadow;
 }
 
