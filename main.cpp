cc59a1a3a8d8a1c6e33488c97d6fb8225825ac5c
Daeho Cho <cdh981009@gmail.com>
2023-11-07 19:37:36 +0900

    Apply anti-aliasing

--- main.cpp
+++ main.cpp
@@ -41,6 +41,7 @@ int main(void) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
     // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
