916a86da641f3d53c627708226cd793d82126874
Daeho Cho <cdh981009@gmail.com>
2023-11-14 00:21:43 +0900

    Use adaptive layer number in steep parallax mapping

--- shaders/parallax_map.fs
+++ shaders/parallax_map.fs
@@ -61,7 +61,9 @@ vec2 parallaxMapping(vec2 texCoords, vec3 viewDir) {
 		return texCoords;
 	}
 
-	const float numLayers = 10;
+	const float minLayers = 8.0;
+	const float maxLayers = 32.0;
+	float numLayers = mix(maxLayers, minLayers, max(dot(vec3(0.0, 0.0, 1.0), viewDir), 0.0));
 	float layerDepth = 1.0 / numLayers;
 	float currentLayerDepth = 0.0;
 
